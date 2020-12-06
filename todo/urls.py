bbe069862777ab67838443e8b4717e56b4ae07b9
Tahara <wants@studiom-web.net>
2020-12-06 14:21:58 +0900

    log in

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import todo, signupview
+from .views import todo, signupview, loginview
 
 urlpatterns = [
     path('signup/', signupview, name='signup'),
+    path('login/', loginview, name='login'),
     path('test/', todo)
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 
 def todo(request):
@@ -19,3 +20,17 @@ def signupview(request):
     else:
         return render(request, 'signup.html', {})
     return render(request, 'signup.html', {})
+
+
+def loginview(request):
+    if request.method == 'POST':
+        username_data = request.POST['username_data']
+        password_data = request.POST['password_data']
+        user = authenticate(request, username=username_data,
+                            password=password_data)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return redirect('login')
+    return render(request, 'login.html')
