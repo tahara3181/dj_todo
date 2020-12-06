0d01160a4c64f8c5e0635cf2a3eb7bf36604592b
Tahara <wants@studiom-web.net>
2020-12-06 12:29:17 +0900

    データベーステーブル作成と稼働

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+PRIORITY = (('danger', 'high'), ('info', 'normal'), ('success', 'low'))
+
+
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField()
+    auther = models.CharField(max_length=100)
+    priority = models.CharField(
+        max_length=50,
+        choices=PRIORITY,
+    )
+    progress = models.PositiveSmallIntegerField()
+    duedate = models.DateField()
+    complete = models.BooleanField()
+
+    def __str__(self):
+        return self.title
